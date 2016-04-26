873741565b75b74b37affa4619d48397b9cc3d57
Benjie Songsong <benjiesongsong@gmail.com>
2016-04-26 14:28:56 -0400

    Working on CSS

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,7 @@ var url = 'https://api.github.com/users/benjiecodes';
 $.getJSON(url).then(function(event){
   console.log(event);
   var html = getHeaderName(event);
-  $('header').append(html);
+  $('.headerName').append(html);
   var html = getInfo(event);
   $('.infoContainer').append(html);
   var html = getAvatar(event)
@@ -14,28 +14,28 @@ $.getJSON(url).then(function(event){
 
 
 function getInfo (info) {
-
+  if (info.company === null) {
+    info.company = '"Currenly Unavailable"';
+  }
   return `
   <ul class="info">
-    <li class="name">Name: ${info.name} </li>
-    <li class="githubURL">Github URL: ${info.url}</li>
-    <li class="email">Email: ${info.email}</li>
-    <li class="company">Company: ${info.company}</li>
-    <li class="website">Website: ${info.html_url}</li>
+    <li class="name"><span>Name: </span> ${info.name} </li>
+    <li class="githubURL"><span>Github URL: </span> ${info.url}</li>
+    <li class="email"><span>Email: </span> ${info.email}</li>
+    <li class="company"><span>Company: </span>${info.company}</li>
+    <li class="website"><span>Website: </span>${info.html_url}</li>
   </ul>
 
   `;
 }
 
 function getHeaderName (info) {
-
   return `
     <h1>${info.name}</h1>
   `;
 }
 
 function getAvatar (info) {
-
   return `
     <img src="${info.avatar_url}">
   `;
